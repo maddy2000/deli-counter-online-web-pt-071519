71a2b853585a67d127caee06e43d2adc0d8f7262
Madelyn Deshield <madelynwallop@gmail.com>
2019-08-04 20:38:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -25,11 +25,12 @@ end
 end
 
 def now_serving(katz_deli)
- if katz_deli.length == 0
-puts "There is nobody waiting to be served!"
-else katz_deli.each_with_index do |name, index|
-  puts katz_deli.push(name)
+  if katz_deli.length == 0
+  puts "There is nobody waiting to be served!"
+  else 
+  name= katz_deli.shift
+  
  
  end
  end
- end
+ 
