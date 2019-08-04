ac051ade50ed18ae1aed728ea6ecab0b2501cf3a
Madelyn Deshield <madelynwallop@gmail.com>
2019-08-04 20:11:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli.length == 0
+ puts "The line is currently empty."
+ end
+end
+
+
+ def take_a_number(katz_deli, name)
+   katz_deli = katz_deli.push(name)
+   if katz_deli.length == 0
+     
+     puts "Welcome, #{katz_deli[0]}. You are number #{katz_deli.count} in line."
+ else 
+  puts "Welcome, #{katz_deli[-1]}. You are number #{katz_deli.count} in line."
+end
+end
+
+def now_serving(katz_deli, name)
+ if katz_deli.length == 0
+puts "the line is empty"
+else katz_deli.each_with_index do |name, index|
+  puts katz_deli.push(name)
+ 
+ end
+ end
+ end
